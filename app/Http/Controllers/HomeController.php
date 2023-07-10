60cb79aa439aaf4f4c7943ea1f9148d75a409195
Hussein Aprianto <105715011+HusseinHato@users.noreply.github.com>
2023-07-10 11:46:00 +0700

    minor bug fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -24,7 +24,7 @@ class HomeController extends Controller
     public function index()
     {
         // return view('/pegawai');
-        return view('/manajemen');
+        return view('home');
         // return view('home');
     }
 }
